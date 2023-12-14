805e33425f7ad977304991f543f7cd29c6394272
unknown <hanboy587@gmail.com>
2023-12-14 14:05:54 +0900

    jwt

---
+++ board-back/src/main/java/com/jun/boardback/provider/JwtProvider.java
@@ -0,0 +1,45 @@
+package com.jun.boardback.provider;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtProvider {
+    
+    private String secretKey = "SecretK3y";
+
+    public String create(String eamil) {
+
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        
+        String jwt = Jwts.builder()
+            .signWith(SignatureAlgorithm.ES256, secretKey)
+            .setSubject(eamil).setIssuedAt(new Date()).setExpiration(expiredDate)
+            .compact();
+
+        return jwt;
+    }
+
+    public String validate(String jwt) {
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(jwt).getBody();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+    }
+
+}
