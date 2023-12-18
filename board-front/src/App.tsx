2a4f554ee93cb7f0c0117f439509790f020cd595
unknown <hanboy587@gmail.com>
2023-12-18 13:26:47 +0900

    Routes 경로 완료

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -5,12 +5,32 @@ import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/commentItem';
 import FavoriteItem from 'components/favoriteItem';
 import InputBox from 'components/inputBox';
+import Footer from 'layouts/footer';
+import { Route, Routes } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
 
+
+// component: Application 컴포넌트 //
 function App() {
-
-  const [value, setValue] = useState<string>('');
-
-  return (
+  
+  
+// render: Application 컴포넌트 렌더링 //
+// description: 메인 : '/' -Main //
+// description: 로그인, 회원가입 : '/auth' - Authentication //
+// description: 검색 : '/search/:searchWord' - Search //
+// description: 유저 : 'user/:userEmail' - User //
+//
+// description: 게시물 상세보기 : '/board/detail/:boardNumber' - BoardDetail //
+// description: 게시물 작성하기 : '/board/write' - BoardWrite //
+// description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate //
+//
+return (
     <>
       {/* 게시물 리스트 */}
       {/* {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />)} */}
@@ -31,7 +51,23 @@ function App() {
       </div> */}
 
       {/* 입력폼 */}
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='aaa' />
+      {/* <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='aaa' /> */}
+
+      {/* Footer */}
+      {/* <Footer /> */}
+
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/auth' element={<Authentication />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/user' element={<User />} />
+        <Route path='/board'>
+        <Route path='write' element={<BoardWrite />} />
+        <Route path='detail/:boardNumber' element={<BoardDetail />} />
+        <Route path='update/:boardNumber' element={<BoardUpdate />} />
+
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ board-front/src/views/Search/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+// component: 검색 컴포넌트 //
+export default function Search() {
+  // render: 검색 컴포넌트 렌더링 //
+  return (
+    <div>게시물 검색</div>
+  )
+}
