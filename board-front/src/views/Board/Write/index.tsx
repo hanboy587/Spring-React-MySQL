e44556f1d7846f14043752d85b45b33a559e055e
unknown <hanboy587@gmail.com>
2023-12-23 12:44:13 +0900

    게시물 작성 컴포넌트 작업 중

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -5,6 +5,21 @@ import './style.css'
 export default function BoardWrite() {
   // render: 게시물 작성 컴포넌트 렌더링 //
   return (
-    <div>게시물 작성</div>
+    <div id='board-write-wrapper'>
+      <div className='board-write-container'>
+        <div className='board-write-box'>
+          <div className='board-write-title-box'>
+            <input className='board-write-title-input' />
+          </div>
+          <div className='divider'></div>
+          <div className='board-write=content-box'>
+            <textarea className='board-write-content-textarea' />
+            <div className='icon-button'>
+              <div className='icon image-box-light-icon'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
