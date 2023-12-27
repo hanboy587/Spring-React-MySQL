48d0d13f6ad86a1ed5b8050b902758e4559fabd7
unknown <hanboy587@gmail.com>
2023-12-27 11:57:29 +0900

    상세 페이지 댓글 달기 기능 구현 중

---
+++ board-front/src/components/Pagination/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css';
+
+// component: pagination 컴포넌트 //
+export default function pagination() {
+
+// render: pagination 컴포넌트 렌더링 //
+  return (
+    <div>index</div>
+  )
+}
