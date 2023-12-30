10a888850706b0d51de998eea8c429ec96902eb6
unknown <hanboy587@gmail.com>
2023-12-30 21:14:21 +0900

    페이징 작업 중 - 페이징 컴포넌트 style 완료, event handler 작업중

--- board-front/src/components/Pagination/index.tsx
+++ board-front/src/components/Pagination/index.tsx
@@ -4,8 +4,32 @@ import './style.css';
 // component: pagination 컴포넌트 //
 export default function pagination() {
 
+  // event handler: pageNumber click event //
+  const onPageNumberClickHandler = () => {
+    
+  }
+
 // render: pagination 컴포넌트 렌더링 //
   return (
-    <div></div>
+    <div id='pagination-wrapper'>
+      <div className='pagination-change-link-box'>
+        <div className='icon-box-small'>
+          <div className='icon expand-left-icon'></div>
+        </div>
+        <div className='pagination-change-link-text'>{'이전'}</div>
+      </div>
+      <div className='pagination-divider'>{'\|'}</div>
+
+      <div className='pagination-text-active'>{1}</div>
+      <div className='pagination-text'>{2}</div>
+
+      <div className='pagination-divider'>{'\|'}</div>
+      <div className='pagination-change-link-box'>
+      <div className='pagination-change-link-text'>{'다음'}</div>
+        <div className='icon-box-small'>
+          <div className='icon expand-right-icon'></div>
+        </div>
+      </div>
+    </div>
   )
 }
